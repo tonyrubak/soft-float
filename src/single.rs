b86da7d1307eac7e0df02b5558406bbb84f90ac9
Joe <joe@crimsonhexagon.us>
2018-03-06 20:15:37 -0500

    implemented shift_and_round and tests

--- src/single.rs
+++ src/single.rs
@@ -38,6 +38,50 @@ pub fn fpsub(x: Single, y: Single) -> Single {
     fpadd(x, y ^ 0x80000000)
 }
 
+// Shift the given mantissa to the right n bits; rounds according to IEEE 754 rules
+//
+// * Truncate if last bit shifted was 0
+// * Increase mantissa by 1 if last bit shifted was 1 and at least one other 1 shifted out
+// * Increase mantissa by 1 if last bit shifted was 1, all other bits were 0, and mantissa LO bit is 1
+
+// Masks for shift_and_round to isolate shifted bits
+
+const MASKS: [u32; 24] = [0x0, 0x1, 0x3, 0x7,
+                          0xF, 0x1F, 0x3F, 0x7F,
+                          0xFF, 0x1FF, 0x3FF, 0x7FF,
+                          0xFFF, 0x1FFF, 0x3FFF, 0x7FFF,
+                          0xFFFF, 0x1FFFF, 0x3FFFF, 0x7FFF,
+                          0xFFFFF, 0x1FFFFF, 0x3FFFFF, 0x7FFFFF,];
+
+const HO_MASKS: [u32; 24] = [0x0,
+                             0x1, 0x2, 0x4, 0x8,
+                             0x10, 0x20, 0x40, 0x80,
+                             0x100, 0x200, 0x400, 0x800,
+                             0x1000, 0x2000, 0x4000, 0x8000,
+                             0x10000, 0x20000, 0x40000, 0x80000,
+                             0x100000, 0x200000, 0x400000];
+
+fn shift_and_round(mantissa: u32, n: usize) -> u32 {
+    assert!(n <= 23);
+
+    let shifted = mantissa & MASKS[n];
+
+    let val = mantissa >> n;
+
+    // Round as needed
+    if shifted > HO_MASKS[n] {
+        // The last bit shifted was a 1, and we shifted out at least 1 other 1, so round up
+        val + 1
+    } else if shifted == HO_MASKS[n] {
+        // The last bit shifted was 1, but we didn't shift out any other 1s,
+        // so round up if LO bit is 1
+        val + (val & 1)
+    } else {
+        // The last bit shifted out was 0, so leave it alone
+        val
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Single;
@@ -70,5 +114,20 @@ mod tests {
     #[test]
     fn is_mantissa_0xae147b() {
         assert_eq!(extract_mantissa!(0x3DAE147B), 0xAE147Bu32);
+    }
+
+    #[test]
+    fn is_mantissa_rounded_up_case_1() {
+        assert_eq!(super::shift_and_round(0x3DAE147B, 4), 0x3DAE148);
+    }
+
+    #[test]
+    fn is_mantissa_rounded_up_case_2() {
+        assert_eq!(super::shift_and_round(0x3DAE1478, 4), 0x3DAE148);
+    }
+
+    #[test]
+    fn is_mantissa_truncated() {
+        assert_eq!(super::shift_and_round(0x3DAE1473, 4), 0x3DAE147);
     }
 }
